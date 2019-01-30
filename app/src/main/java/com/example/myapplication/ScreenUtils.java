c4b10312df18ee339cea7fffeaacc6166989b260
ashishkdataback <ashish.k@databackapp.com>
2019-01-30 08:46:04 +0530

    initial commit

---
+++ app/src/main/java/com/example/myapplication/ScreenUtils.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+public final class ScreenUtils {
+
+    private ScreenUtils() {
+        // This class is not publicly instantiable
+    }
+
+    public static int getScreenHeight(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(dm);
+        return dm.heightPixels;
+    }
+
+    public static int getScreenWidth(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(dm);
+        return dm.widthPixels;
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/Wallpaper.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+public class Wallpaper {
+
+   private int height;
+   private int width;
+   private String imageUrl;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Wallpaper(int height, int width, String imageUrl) {
+        this.height = height;
+        this.width = width;
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Wallpaper{" +
+                "height=" + height +
+                ", width=" + width +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
